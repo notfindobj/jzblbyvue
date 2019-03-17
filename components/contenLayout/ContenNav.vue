18aeed5abfea7f2c22a8db84e83157e2e3136574
liudeqing <1519464841@qq.com>
2019-03-17 11:52:21 +0800

    add

---
+++ components/contenLayout/ContenNav.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="nav-box">
+    <div class="nav-list">
+      <p>在以下分类中找：</p>
+      <ul>
+        <li>示范区</li>
+        <li>楼盘</li>
+        <li>效果图</li>
+        <li>SU模型</li>
+        <li>总图</li>
+        <li>平面</li>
+        <li>建筑规范</li>
+        <li>室内案例</li>
+        <li>文本</li>
+      </ul>
+    </div>
+    <div class="screening-nav">
+      <ul class="screening-nav-list">
+        <li>
+          <p>类   型：</p>
+          <ol>
+            <li>高层住宅</li>
+            <li>洋房</li>
+            <li>别墅</li>
+            <li>小区入口</li>
+            <li>入口大堂</li>
+            <li>售楼处</li>
+            <li>商业综合体</li>
+            <li>大沿街商业</li>
+            <li>高层办公</li>
+            <li>多层办公</li>
+            <li>园区办公</li>
+            <li>高层酒店</li>
+
+          </ol>
+        </li>
+        <li>
+          <p>风   格：</p>
+          <ol>
+            <li>高层住宅</li>
+            <li>洋房</li>
+            <li>别墅</li>
+            <li>小区入口</li>
+            <li>入口大堂</li>
+            <li>售楼处</li>
+            <li>商业综合体</li>
+            <li>大沿街商业</li>
+            <li>高层办公</li>
+            <li>多层办公</li>
+            <li>园区办公</li>
+            <li>高层酒店</li>
+          </ol>
+        </li>
+        <li>
+          <p>色   调：</p>
+          <ol>
+            <li>高层住宅</li>
+            <li>洋房</li>
+            <li>别墅</li>
+            <li>小区入口</li>
+            <li>入口大堂</li>
+            <li>售楼处</li>
+            <li>商业综合体</li>
+            <li>大沿街商业</li>
+            <li>高层办公</li>
+            <li>多层办公</li>
+            <li>园区办公</li>
+            <li>高层酒店</li>
+          </ol>
+        </li>
+        <li>
+          <p>公司区位：</p>
+          <ol>
+            <li>高层住宅</li>
+            <li>洋房</li>
+            <li>别墅</li>
+            <li>小区入口</li>
+            <li>入口大堂</li>
+            <li>售楼处</li>
+            <li>商业综合体</li>
+            <li>大沿街商业</li>
+            <li>高层办公</li>
+            <li>多层办公</li>
+            <li>园区办公</li>
+            <li>高层酒店</li>
+          </ol>
+        </li>
+        <li>
+          <p>公   司：</p>
+          <ol>
+            <li>高层住宅</li>
+            <li>洋房</li>
+            <li>别墅</li>
+            <li>小区入口</li>
+            <li>入口大堂</li>
+            <li>售楼处</li>
+            <li>商业综合体</li>
+            <li>大沿街商业</li>
+            <li>高层办公</li>
+            <li>多层办公</li>
+            <li>园区办公</li>
+            <li>高层酒店</li>
+          </ol>
+        </li>
+        <li>
+          <p>其他效果图：</p>
+          <ol>
+            <li>高层住宅</li>
+            <li>洋房</li>
+            <li>别墅</li>
+            <li>小区入口</li>
+            <li>入口大堂</li>
+            <li>售楼处</li>
+            <li>商业综合体</li>
+            <li>大沿街商业</li>
+            <li>高层办公</li>
+            <li>多层办公</li>
+            <li>园区办公</li>
+            <li>高层酒店</li>
+          </ol>
+        </li>
+      </ul>
+    </div>
+    <Breadcrumb separator=">" style="margin-bottom: 20px">
+      <BreadcrumbItem style="color: #999999">资源库</BreadcrumbItem>
+      <BreadcrumbItem style="color: #999999;font-weight: normal">效果图</BreadcrumbItem>
+    </Breadcrumb>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'contenNav',
+    data() {
+      return {}
+    },
+    mounted() {
+    },
+    methods: {
+    },
+  }
+</script>
+<style lang="less" scoped>
+  .nav-box{
+    width: 100%;
+    height: auto;
+    padding-top: 20px;
+    .nav-list{
+      font-size: 16px;
+      color: #333333;
+      height: 22px;
+      margin-bottom: 4px;
+      display: flex;
+      flex-direction: row;
+      >p{
+        width: 128px;
+        margin-right: 14px;
+      }
+      >ul{
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        >li{
+          height: 22px;
+          padding-right: 13px;
+          margin-right: 13px;
+          position: relative;
+          cursor: pointer;
+          &:before{
+            width: 2px;
+            height: 10px;
+            content: '';
+            background: #D6D6D6;
+            border-radius: 1px;
+            position: absolute;
+            right: 0;
+            top:6px;
+          }
+          &:last-child{
+            &:before{
+              width: 0px;
+              height: 10px;
+              content: '';
+              background: #D6D6D6;
+              border-radius: 1px;
+              position: absolute;
+              right: 0;
+              top:6px;
+            }
+          }
+          &:hover{
+            color: #FF3C00;
+          }
+        }
+      }
+    }
+    .screening-nav{
+      width: 100%;
+      height: auto;
+      margin-top: 12px;
+      margin-bottom: 10px;
+      background: #FFFFFF;
+      border: 1px solid #303030;
+      border-radius: 4px;
+      box-sizing: border-box;
+      padding: 0px 20px;
+      .screening-nav-list{
+        width: 100%;
+        >li{
+          line-height: 40px;
+          font-size: 14px;
+          color: #333333;
+          border-bottom: 1px dashed #DDDDDD;
+          box-sizing: border-box;
+          display: flex;
+          flex-direction: row;
+          &:last-child{
+            border-bottom: none;
+          }
+          >p{
+            /*width: 76px;*/
+            margin-right: 20px;
+            font-weight: 500;
+          }
+          >ol{
+            flex: 1;
+
+            >li{
+              float: left;
+              margin-right: 24px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .ivu-breadcrumb-item-separator{
+    margin: 0 2px!important;
+  }
+</style>
---
+++ components/contenLayout/Content.vue
@@ -0,0 +1,471 @@
+<template>
+  <div class="content-box">
+    <ul class="content-screening-list">
+      <li class="li-active">最新上传</li>
+      <li>人气</li>
+      <li>下载</li>
+      <li>收藏</li>
+    </ul>
+    <ul class="works-list">
+      <li>
+        <div class="img-box">
+          <img src="../../assets/images/worksImg.png" alt="">
+          <div class="works-like">
+            <p>
+              35588
+            </p>
+            <p>
+              收藏
+            </p>
+          </div>
+        </div>
+        <div class="works-introduce">
+          <p>广东江门龙溪湖设计示范区</p>
+          <div>
+            <img src="../../assets/images/a.jpeg" alt="">
+            <span>
+              杨小幂
+            </span>
+          </div>
+        </div>
+        <div class="works-con">
+          <img src="../../assets/images/a.jpeg" alt="">
+          <p>梅赛德斯·赵四</p>
+          <ul class="works-con-introduce">
+            <li>
+              <p>项目</p>
+              <p>1334</p>
+            </li>
+            <li>
+              <p>粉丝</p>
+              <p>1.3万</p>
+            </li>
+          </ul>
+          <div class="btn-group-box">
+            <div>关注</div>
+            <div>私信</div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="img-box">
+          <img src="../../assets/images/worksImg.png" alt="">
+          <div class="works-like">
+            <p>
+              35588
+            </p>
+            <p>
+              收藏
+            </p>
+          </div>
+        </div>
+        <div class="works-introduce">
+          <p>广东江门龙溪湖设计示范区</p>
+          <div>
+            <img src="../../assets/images/a.jpeg" alt="">
+            <span>
+              杨小幂
+            </span>
+          </div>
+        </div>
+        <div class="works-con">
+          <img src="../../assets/images/a.jpeg" alt="">
+          <p>梅赛德斯·赵四</p>
+          <ul class="works-con-introduce">
+            <li>
+              <p>项目</p>
+              <p>1334</p>
+            </li>
+            <li>
+              <p>粉丝</p>
+              <p>1.3万</p>
+            </li>
+          </ul>
+          <div class="btn-group-box">
+            <div>关注</div>
+            <div>私信</div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="img-box">
+          <img src="../../assets/images/worksImg.png" alt="">
+          <div class="works-like">
+            <p>
+              35588
+            </p>
+            <p>
+              收藏
+            </p>
+          </div>
+        </div>
+        <div class="works-introduce">
+          <p>广东江门龙溪湖设计示范区</p>
+          <div>
+            <img src="../../assets/images/a.jpeg" alt="">
+            <span>
+              杨小幂
+            </span>
+          </div>
+        </div>
+        <div class="works-con">
+          <img src="../../assets/images/a.jpeg" alt="">
+          <p>梅赛德斯·赵四</p>
+          <ul class="works-con-introduce">
+            <li>
+              <p>项目</p>
+              <p>1334</p>
+            </li>
+            <li>
+              <p>粉丝</p>
+              <p>1.3万</p>
+            </li>
+          </ul>
+          <div class="btn-group-box">
+            <div>关注</div>
+            <div>私信</div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="img-box">
+          <img src="../../assets/images/worksImg.png" alt="">
+          <div class="works-like">
+            <p>
+              35588
+            </p>
+            <p>
+              收藏
+            </p>
+          </div>
+        </div>
+        <div class="works-introduce">
+          <p>广东江门龙溪湖设计示范区</p>
+          <div>
+            <img src="../../assets/images/a.jpeg" alt="">
+            <span>
+              杨小幂
+            </span>
+          </div>
+        </div>
+        <div class="works-con">
+          <img src="../../assets/images/a.jpeg" alt="">
+          <p>梅赛德斯·赵四</p>
+          <ul class="works-con-introduce">
+            <li>
+              <p>项目</p>
+              <p>1334</p>
+            </li>
+            <li>
+              <p>粉丝</p>
+              <p>1.3万</p>
+            </li>
+          </ul>
+          <div class="btn-group-box">
+            <div>关注</div>
+            <div>私信</div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="img-box">
+          <img src="../../assets/images/worksImg.png" alt="">
+          <div class="works-like">
+            <p>
+              35588
+            </p>
+            <p>
+              收藏
+            </p>
+          </div>
+        </div>
+        <div class="works-introduce">
+          <p>广东江门龙溪湖设计示范区</p>
+          <div>
+            <img src="../../assets/images/a.jpeg" alt="">
+            <span>
+              杨小幂
+            </span>
+          </div>
+        </div>
+        <div class="works-con">
+          <img src="../../assets/images/a.jpeg" alt="">
+          <p>梅赛德斯·赵四</p>
+          <ul class="works-con-introduce">
+            <li>
+              <p>项目</p>
+              <p>1334</p>
+            </li>
+            <li>
+              <p>粉丝</p>
+              <p>1.3万</p>
+            </li>
+          </ul>
+          <div class="btn-group-box">
+            <div>关注</div>
+            <div>私信</div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="img-box">
+          <img src="../../assets/images/worksImg.png" alt="">
+          <div class="works-like">
+            <p>
+              35588
+            </p>
+            <p>
+              收藏
+            </p>
+          </div>
+        </div>
+        <div class="works-introduce">
+          <p>广东江门龙溪湖设计示范区</p>
+          <div>
+            <img src="../../assets/images/a.jpeg" alt="">
+            <span>
+              杨小幂
+            </span>
+          </div>
+        </div>
+        <div class="works-con">
+          <img src="../../assets/images/a.jpeg" alt="">
+          <p>梅赛德斯·赵四</p>
+          <ul class="works-con-introduce">
+            <li>
+              <p>项目</p>
+              <p>1334</p>
+            </li>
+            <li>
+              <p>粉丝</p>
+              <p>1.3万</p>
+            </li>
+          </ul>
+          <div class="btn-group-box">
+            <div>关注</div>
+            <div>私信</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <Page :total="100" show-sizer />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'conten',
+    data() {
+      return {}
+    },
+    mounted() {
+    },
+    methods: {
+    },
+  }
+</script>
+<style lang="less" scoped>
+  .clearfix() {
+    &:before,
+    &:after {
+      content: " ";
+      display: table;
+    }
+    &:after {
+      clear: both;
+    }
+  }
+  .content-box{
+    width: 100%;
+    height: auto;
+    .content-screening-list{
+      display: flex;
+      flex-direction: row;
+      margin-bottom: 23px;
+      >li{
+        line-height: 20px;
+        font-size: 14px;
+        color: #666666;
+        margin-right: 36px;
+      }
+      .li-active{
+        color: #ED1B24;
+        font-weight: 500;
+        position: relative;
+        &:before{
+          width: 30px;
+          height: 1px;
+          content: '';
+          background: #ED1B24;
+          border-radius: 2px;
+          position: absolute;
+          left: 50%;
+          bottom:-2px;
+          transform: translateX(-50%);
+        }
+      }
+    }
+    .works-list{
+      width: 100%;
+      .clearfix();
+      >li{
+        float: left;
+        width: 292px;
+        height: 266px;
+        background: #FFFFFF;
+        box-shadow: 0 2px 20px 0 rgba(0,0,0,0.15);
+        border-radius: 4px;
+        margin-right: 10px;
+        margin-bottom: 20px;
+        position: relative;
+        &:nth-child(4n){
+          margin-right: 0;
+        }
+        .img-box{
+          height: 200px;
+          position: relative;
+          >img{
+            display: inline-block;
+            width: 100%;
+            height: auto;
+          }
+          .works-like{
+            width: 100%;
+            height: 40px;
+            padding-top: 16px;
+            padding-left: 6px;
+            padding-right: 6px;
+            opacity: 0.6;
+            background: #000000;
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            >p{
+              font-size: 12px;
+              color: #FFFFFF;
+              &:nth-child(1){
+                float: left;
+              }
+              &:nth-child(2){
+                float: right;
+              }
+            }
+          }
+        }
+        .works-introduce{
+          height: 66px;
+          padding: 8px 0 10px 6px;
+          >p{
+            font-size: 14px;
+            color: #666666;
+            margin-bottom: 8px;
+          }
+          >div{
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            align-items:flex-start;
+            img{
+              display: inline-block;
+              width: 20px;
+              height: 20px;
+              border-radius: 50%;
+              margin-right: 4px;
+            }
+            >span{
+              font-size: 14px;
+              color: #333333;
+            }
+          }
+        }
+        .works-con{
+          width: 163px;
+          height: 217px;
+          padding: 20px 16px;
+          background: #FFFFFF;
+          box-shadow: 0 2px 10px 0 rgba(0,0,0,0.10);
+          border-radius: 4px;
+          position: absolute;
+          left: 45px;
+          top:7px;
+          >img{
+            display: block;
+            width: 64px;
+            height: 64px;
+            border-radius: 50%;
+            background: red;
+            margin: 0 auto 10px;
+          }
+          >p{
+            font-size: 14px;
+            color: #333333;
+            margin-bottom: 12px;
+            text-align: center;
+          }
+          .works-con-introduce{
+            height: 40px;
+            margin-bottom: 12px;
+            display: flex;
+            flex-direction: row;
+            position: relative;
+            >li{
+              flex: 1;
+              >p{
+                &:nth-child(1){
+                  font-size: 12px;
+                  color: #999999;
+                  text-align: center;
+                }
+                &:nth-child(2){
+                  font-size: 12px;
+                  color: #333333;
+                  text-align: center;
+                }
+              }
+            }
+            &:before{
+              width: 2px;
+              height: 20px;
+              content: '';
+              background: #D9D9D9;
+              border-radius: 2px;
+              position: absolute;
+              left: 50%;
+              top:10px;
+              transform: translateX(-50%);
+            }
+          }
+          .btn-group-box{
+            height: 21px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            >div{
+              width: 60px;
+              height: 21px;
+              border-radius: 4px;
+              box-sizing: border-box;
+              text-align: center;
+              line-height: 21px;
+              font-size: 12px;
+              &:nth-child(1){
+                color: #ffffff;
+                background: #FF3C00 ;
+              }
+              &:nth-child(1){
+                color: #ffffff;
+                background: #FF3C00 ;
+              }
+              &:nth-child(2){
+                border: 1px solid #D9D9D9;
+                background: #ffffff;
+                color: #666666;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
