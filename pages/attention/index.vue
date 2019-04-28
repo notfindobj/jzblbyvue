1706859868836eb7a263e40046d099c9334c0121
hzpeng <hrrmzp@163.com>
2019-04-28 21:24:42 +0800

    关注页面

--- pages/attention/index.vue
+++ pages/attention/index.vue
@@ -24,7 +24,174 @@
             <div class="content">
                 <p>新一季的《最强大脑》，依然汇聚了清华北大海外高校的各路牛人，高智商与高智商的碰撞，让我看得十分过瘾。在这巅峰的碰撞中，我意外捕捉到了比超越脑力还震撼的一幕。初始100强里，不是20几岁的高材生，就是10几岁的神童们，非常意外地，居然还有一个40岁的“大叔”来参赛。要知道，这场顶级脑力的比拼对中年人可一点不友好。</p>
             </div>
-            <div class="block-foot"></div>
+            <div class="block-foot">
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe696;</i>
+                    <span>收藏</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe6be;</i>
+                    <span>12</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe664;</i>
+                    <span>评论</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe67e;</i>
+                    <span>点赞</span>
+                </div>
+            </div>
+        </div>
+        <div class="public-block">
+            <div class="block-head">
+                <div class="block-head-left">
+                    <div class="avatar"></div>
+                    <div class="info">
+                        <p class="name">杨小幂</p>
+                        <p class="time">3分钟前</p>
+                    </div>
+                </div>
+                <div class="block-head-right">
+                    <Dropdown placement="bottom-end" trigger="click">
+                        <a href="javascript:void(0)">
+                            <Icon type="ios-arrow-down"></Icon>
+                        </a>
+                        <DropdownMenu slot="list">
+                            <DropdownItem>帮上头条</DropdownItem>
+                            <DropdownItem>投诉</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+            </div>
+            <div class="content">
+                <p>新一季的《最强大脑》，依然汇聚了清华北大海外高校的各路牛人，高智商与高智商的碰撞，让我看得十分过瘾。在这巅峰的碰撞中，我意外捕捉到了比超越脑力还震撼的一幕。初始100强里，不是20几岁的高材生，就是10几岁的神童们，非常意外地，居然还有一个40岁的“大叔”来参赛。要知道，这场顶级脑力的比拼对中年人可一点不友好。</p>
+                <div class="photo-wrap">
+                    <div class="img"></div>
+                    <div class="img"></div>
+                    <div class="img"></div>
+                    <div class="img"></div>
+                </div>
+            </div>
+            <div class="block-foot">
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe696;</i>
+                    <span>收藏</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe6be;</i>
+                    <span>12</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe664;</i>
+                    <span>评论</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe67e;</i>
+                    <span>点赞</span>
+                </div>
+            </div>
+        </div>
+        <div class="public-block recommend">
+            <div class="recommend-title">
+                <i class="icon iconfont">&#xe60b;</i>
+                <span>推荐</span>
+            </div>
+            <div class="recommend-list">
+                <div class="recommend-item">
+                    <div class="img-wrap"></div>
+                    <div class="name">
+                        张小杰
+                        <div class="hidden-box">
+                            <div class="avatar"></div>
+                            <p class="user-name">杨小幂</p>
+                            <div class="user-data">
+                                <div class="data data-left">
+                                    <span class="data-name">项目</span>
+                                    <span class="data-value">1334</span>
+                                </div>
+                                <div class="line"></div>
+                                <div class="data data-left">
+                                    <span class="data-name">粉丝</span>
+                                    <span class="data-value">1.3万</span>
+                                </div>
+                            </div>
+                            <div class="hidden-btns">
+                                <Button size="small" type="primary">关注</Button>
+                                <Button size="small">私信</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="info">一个有态度的设计师，给你精彩</p>
+                </div>
+                <div class="recommend-item">
+                    <div class="img-wrap"></div>
+                    <div class="name">张小杰</div>
+                    <p class="info">一个有态度的设计师，给你精彩</p>
+                </div>
+                <div class="recommend-item">
+                    <div class="img-wrap"></div>
+                    <div class="name">张小杰</div>
+                    <p class="info">一个有态度的设计师，给你精彩</p>
+                </div>
+                <div class="recommend-item">
+                    <div class="img-wrap"></div>
+                    <div class="name">张小杰</div>
+                    <p class="info">一个有态度的设计师，给你精彩</p>
+                </div>
+                <div class="to-pre">
+                    <i class="icon iconfont">&#xe62e;</i>
+                </div>
+                <div class="to-next">
+                    <i class="icon iconfont">&#xe62e;</i>
+                </div>
+            </div>
+        </div>
+        <div class="public-block">
+            <div class="block-head">
+                <div class="block-head-left">
+                    <div class="avatar"></div>
+                    <div class="info">
+                        <p class="name">杨小幂</p>
+                        <p class="time">3分钟前</p>
+                    </div>
+                </div>
+                <div class="block-head-right">
+                    <Dropdown placement="bottom-end" trigger="click">
+                        <a href="javascript:void(0)">
+                            <Icon type="ios-arrow-down"></Icon>
+                        </a>
+                        <DropdownMenu slot="list">
+                            <DropdownItem>帮上头条</DropdownItem>
+                            <DropdownItem>投诉</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+            </div>
+            <div class="content">
+                <p>新一季的《最强大脑》，依然汇聚了清华北大海外高校的各路牛人，高智商与高智商的碰撞，让我看得十分过瘾。在这巅峰的碰撞中，我意外捕捉到了比超越脑力还震撼的一幕。初始100强里，不是20几岁的高材生，就是10几岁的神童们，非常意外地，居然还有一个40岁的“大叔”来参赛。要知道，这场顶级脑力的比拼对中年人可一点不友好。</p>
+                <div class="video-wrap">
+                    <div class="video"></div>
+                </div>
+            </div>
+            <div class="block-foot">
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe696;</i>
+                    <span>收藏</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe6be;</i>
+                    <span>12</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe664;</i>
+                    <span>评论</span>
+                </div>
+                <div class="foot-child">
+                    <i class="icon iconfont">&#xe67e;</i>
+                    <span>点赞</span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -41,10 +208,11 @@
     margin: 30px auto;
     .public-block {
         width: 100%;
+        margin-bottom: 10px;
+        padding-top: 30px;
         background-color: #fff;
         box-shadow: 0 1px 3px 0 rgba(0,0,0,0.10);
         border-radius: 2px;
-        padding-top: 30px;
         .block-head {
             display: flex;
             justify-content: space-between;
@@ -77,10 +245,207 @@
                 font-size: 14px;
                 color: #333;
             }
+            .photo-wrap {
+                display: flex;
+                padding-top: 18px;
+                .img {
+                    width: 240px;
+                    height: 240px;
+                    margin-right: 10px;
+                    background-color: #ccc;
+                }
+            }
+            .video-wrap {
+                padding-top: 18px;
+                .video {
+                    width: 460px;
+                    height: 300px;
+                    background-color: #ccc;
+                }
+
+            }
         }
         .block-foot {
             height: 36px;
             border-top: 1px solid #d8d8d8;
+            display: flex;
+            align-items: center;
+            .foot-child {
+                width: 25%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                border-right: 1px solid #d8d8d8;
+                font-size: 14px;
+                color: #666;
+                i {
+                    margin-right: 4px;
+                    cursor: pointer;
+                }
+                span {
+                    cursor: pointer;
+                }
+            }
+
+            .foot-child:last-child {
+                border-right: none;
+            }
+        }
+    }
+    .recommend {
+        padding: 30px;
+        .recommend-title {
+            span {
+                font-size: 30px;
+                color: #333;
+            }
+            i {
+                margin-right: 5px;
+                font-size: 28px;
+                color: #ff3c00;
+            }
+        }
+        .recommend-list {
+            position: relative;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            margin-top: 30px;
+            .recommend-item {
+                width: 267px;
+                height: 371px;
+                box-shadow: 0 2px 10px 0 rgba(0,0,0,0.10);
+                border-radius: 6px;
+                overflow: hidden;
+                cursor: pointer;
+                .img-wrap {
+                    width: 100%;
+                    height: 285px;
+                    background-color: #ccc;
+                }
+                .name, .info {
+                    padding-left: 15px;
+                }
+                .name {
+                    position: relative;
+                    margin-top: 14px;
+                    font-size: 18px;
+                    color: #333;
+                }
+                .info {
+                    margin-top: 5px;
+                    font-size: 14px;
+                    color: #666;
+                }
+                .hidden-box {
+                    display: none;
+                    position: absolute;
+                    left: 13px;
+                    top: -225px;
+                    width: 163px;
+                    height: 217px;
+                    flex-direction: column;
+                    align-items: center;
+                    background-color: #fff;
+                    box-shadow: 0 2px 10px 0 rgba(0,0,0,0.10);
+                    border-radius: 4px;
+                    z-index: 10;
+                    .avatar {
+                        width: 65px;
+                        height: 65px;
+                        margin-top: 20px;
+                        border-radius: 50%;
+                        border: 1px solid #ccc;
+                    }
+                    .user-name {
+                        margin-top: 10px;
+                        font-size: 14px;
+                        color: #333;
+                    }
+                    .user-data {
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center;
+                        width: 96px;
+                        margin-top: 10px;
+                        .line {
+                            width: 2px;
+                            height: 20px;
+                            background-color: #d9d9d9;
+                        }
+                        .data {
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            .data-name {
+                                font-size: 12px;
+                                color: #999;
+                            }
+                            .data-value {
+                                font-size: 12px;
+                                color: #333;
+                            }
+                        }
+                    }
+                    .hidden-btns {
+                        width: 130px;
+                        display: flex;
+                        justify-content: space-between;
+                        margin-top: 10px;
+                        button {
+                            height: 21px;
+                            width: 60px;
+                            font-size: 12px;
+                            line-height: 16px;
+                        }
+                    }
+                }
+
+                .hidden-box:after {
+                    content: '';
+                    display: block;
+                    border: 10px solid transparent;
+                    border-top-color: #fff;
+                    position: absolute;
+                    bottom: -16px;
+                    left: 2px;
+                }
+            }
+            .name:hover {
+                .hidden-box {
+                    display: flex;
+                }
+            }
+            .to-next, .to-pre {
+                display: none;
+                position: absolute;
+                top: 109px;
+                width: 46px;
+                height: 46px;
+                text-align: center;
+                line-height: 46px;
+                border-radius: 23px 0 0 23px;
+                background: rgba(0,0,0,0.30);
+                cursor: pointer;
+                i {
+                    color: #C5C5C5;
+                    font-size: 20px;
+                }
+            }
+            .to-next {
+                right: 0;
+                border-radius: 23px 0 0 23px;
+            }
+            .to-pre {
+                left: 0;
+                transform: rotate(180deg);
+            }
+        }
+
+        .recommend-list:hover {
+            .to-next, .to-pre {
+                display: block;
+            }
         }
     }
 }
