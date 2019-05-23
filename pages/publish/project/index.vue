62e295391899d6994e7a72a7bc56955372a599a2
hzpeng <hrrmzp@163.com>
2019-05-23 23:47:16 +0800

    发布项目页面，选择类型，定制服务

--- pages/publish/project/index.vue
+++ pages/publish/project/index.vue
@@ -9,10 +9,20 @@
                 :label-width="110"
             >
                 <FormItem label="项目图片" prop="img">
+                    <div class="upload-img-wrap" v-if="formValidate.img">
+                        <img class="title-img" :src="formValidate.img" alt="">
+                    </div>
                     <div class="upload-img-wrap">
-                        <Upload action="" class="upload-component" accept="image/gif,image/jpeg,image/jpg,image/png">
+                        <div class="upload-component">
+                            <a href="javascript:">
+                                <input
+                                    type="file"
+                                    @change="fileSelected($event)"
+                                    accept="image/gif,image/jpeg,image/jpg,image/png"
+                                >
+                            </a>
                             <Icon type="ios-add-circle" size="35" color="#ff3c00"/>
-                        </Upload>
+                        </div>
                         <p>图片将会自动压缩</p>
                         <p>图片建议不要超过20M</p>
                     </div>
@@ -26,19 +36,34 @@
                     ></Input>
                 </FormItem>
                 <FormItem label="类型选择" class="type-select">
-                    <Button size="large">示范区</Button>
-                    <Button size="large">楼盘</Button>
-                    <Button size="large">示范区</Button>
-                    <Button type="primary" size="large">示范区</Button>
+                    <i
+                        v-for="item in typeList"
+                        :key="item.id"
+                    >
+                        <Button
+                            size="large"
+                            v-if="typeId === item.id"
+                            type="primary"
+                            @click="clickType(item.id)"
+                        >{{ item.ItemValue }}
+                        </Button>
+                        <Button
+                            size="large"
+                            v-else
+                            @click="clickType(item.id)"
+                        >{{ item.ItemValue }}
+                        </Button>
+                    </i>
+
                 </FormItem>
                 <FormItem label="定制服务" class="make-service">
-                    <span class="add-service-btn">
+                    <span class="add-service-btn" @click="serviceModal = true">
                         <Icon type="md-add" size="12"/>
                         添加定制服务
                     </span>
                 </FormItem>
                 <FormItem label="项目描述" class="description">
-                    <textarea placeholder="请填写项目描述"></textarea>
+                    <textarea v-model="formValidate.description" placeholder="请填写项目描述"></textarea>
                 </FormItem>
                 <FormItem label="项目内容">
                     <div class="editor-wrap">
@@ -58,25 +83,102 @@
             </p>
             <Button type="primary">完成上传</Button>
         </div>
+        <Spin fix v-if="spinShow">
+            <Icon type="ios-loading" size=18 class="demo-spin-icon-load"></Icon>
+            <div>Loading</div>
+        </Spin>
+
+        <Modal
+            v-model="serviceModal"
+            width="594"
+            title="提供定制服务"
+            class-name="service-box"
+        >
+
+            <div class="service-box-content">
+                <Form
+                    ref="serviceValidate"
+                    :model="serviceValidate"
+                    :rules="ruleServiceValidate"
+                    :label-width="80"
+                >
+                    <FormItem label="定制类型:" prop="type">
+                        <span
+                            class="service-type-name"
+                            :class="{'service-active': item.ItemDetailId === serviceValidate.serviceId}"
+                            v-for="item in serviceList"
+                            :key="item.ItemDetailId"
+                            @click="selectSerice(item.ItemDetailId)"
+                        >{{ item.ItemValue }}</span>
+                    </FormItem>
+
+                    <FormItem label="定制金额:" prop="money">
+                        <Input v-model="serviceValidate.money" placeholder="请选择"></Input>
+                    </FormItem>
+                    <FormItem label="手机号码:" prop="mobile">
+                        <Input v-model="serviceValidate.mobile" placeholder="请输入11位手机号码"></Input>
+                    </FormItem>
+                    <FormItem label="定制描述:" prop="desc">
+                        <Input
+                            v-model="serviceValidate.desc"
+                            type="textarea"
+                            :autosize="{minRows: 5,maxRows: 5}"
+                            placeholder="请填写0-200字简介"
+                        ></Input>
+                    </FormItem>
+                </Form>
+            </div>
+            <div slot="footer">
+                <Button @click="cancelService">取消</Button>
+                <Button type="primary" @click="serviceModal = false">确定</Button>
+            </div>
+        </Modal>
     </div>
 </template>
 
 <script>
+  import { uploadFile, getProjectType, getCustomizeService } from '../../../service/clientAPI'
+
   export default {
     data() {
       return {
+        typeId: '',
+        typeList: [],
+        serviceModal: false,
+        serviceId: '',
         formValidate: {
           name: '',
-          img: ''
+          img: '',
+          content: '',
+          description: ''
         },
         ruleValidate: {
           name: [
-            { required: true, message: 'The name cannot be empty', trigger: 'blur' }
+            { required: true, message: '项目名称不能为空', trigger: 'blur' }
           ],
           img: [
-            { required: true, message: 'The name cannot be empty', trigger: 'blur' }
+            { required: true, message: '图片不能为空', trigger: 'blur' }
           ],
         },
+        serviceValidate: {
+          serviceId: '',
+          money: '',
+          mobile: '',
+          desc: ''
+        },
+        ruleServiceValidate: {
+          type: [
+            { required: true, message: ' ', trigger: 'blur' }
+          ],
+          money: [
+            { required: true, message: ' ', trigger: 'blur' }
+          ],
+          desc: [
+            { required: true, message: ' ', trigger: 'blur' }
+          ]
+        },
+        spinShow: false,
+        serviceList: [],
         editorOption: {
           modules: {
             toolbar: [
@@ -101,7 +203,55 @@
       }
     },
     methods: {
-        
+
+      // 选择定制服务
+      selectSerice(id) {
+        this.$set(this.serviceValidate, 'serviceId', id);
+      },
+
+      // 取消定制服务
+      cancelService() {
+        this.serviceModal = false;
+        this.serviceValidate = {
+          type: '',
+          money: '',
+          mobile: '',
+          desc: ''
+        }
+      },
+
+      // 选择图片
+      fileSelected(e) {
+        let file = e.target.files;
+        if (file) {
+          let data = new FormData();
+          for (let item of file) {
+            data.append('files', item)
+          }
+          this.spinShow = true;
+          uploadFile(data, this.uploadType).then(res => {
+            this.spinShow = false;
+            this.$set(this.formValidate, 'img', res.smallImgUrl);
+          }).catch(err => {
+            console.log(err, 'uploadErr')
+          })
+        }
+      },
+
+      // 选择类型
+      clickType(id) {
+        this.typeId = id;
+        getCustomizeService(id).then(res => {
+          this.serviceList = res;
+        })
+      },
+    },
+
+    async asyncData() {
+      const data = await getProjectType();
+      return {
+        typeList: data
+      }
     }
   }
 </script>
@@ -122,10 +272,17 @@
         }
 
         .upload-img-wrap {
+            float: left;
             width: 260px;
             height: 196px;
             background-color: #f5f5f5;
             text-align: center;
+            margin-right: 20px;
+
+            .title-img {
+                width: 100%;
+                height: 100%;
+            }
 
             p {
                 font-size: 12px;
@@ -133,8 +290,36 @@
             }
 
             .upload-component {
-                padding-top: 46px;
+                position: relative;
+                height: 88px;
                 cursor: pointer;
+
+                a {
+                    position: absolute;
+                    top: 40px;
+                    left: 0;
+                    right: 0;
+                    margin: 0 auto;
+                    z-index: 2;
+                    cursor: pointer;
+                }
+
+                i {
+                    position: absolute;
+                    top: 40px;
+                    left: 0;
+                    right: 0;
+                    margin: 0 auto;
+                    z-index: 1;
+                }
+
+                input[type="file"] {
+                    width: 35px;
+                    height: 35px;
+                    opacity: 0;
+                    filter: alpha(opacity=0);
+                    cursor: pointer;
+                }
             }
         }
 
@@ -165,8 +350,9 @@
                 border: 1px solid #d8d8d8;
                 border-radius: 4px;
                 outline: none;
-                resize:none;
+                resize: none;
             }
+
             textarea::placeholder {
                 color: #999;
             }
@@ -175,12 +361,28 @@
         .editor-wrap {
             width: 770px;
         }
+
         .quill-editor {
             height: 318px;
             overflow-y: auto;
             border-radius: 0 0 4px 4px;
             border: 1px solid #d8d8d8;
         }
+    }
+
+    .demo-spin-icon-load {
+        animation: ani-demo-spin 1s linear infinite;
+    }
+
+    .service-type-name {
+        font-size: 12px;
+        color: #666;
+        margin-right: 20px;
+        cursor: pointer;
+    }
+
+    .service-active {
+        color: #ff3c00;
     }
 
 </style>
