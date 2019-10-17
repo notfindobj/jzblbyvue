e6f933f5c79fd00bc88eb4a953fc65513d5194c4
DESKTOP-OVJB0DA\sqliuen <sqliuen@163.com>
2019-10-17 11:55:44 +0800

    add

--- LocalEnv.js
+++ LocalEnv.js
@@ -9,6 +9,7 @@ exports.configUrl = {
             return 'http://www.api.jzbl.com/api/'
         } else {
         // 本地环境
+            // return 'http://192.168.10.13:8066/api/'
             return 'http://106.14.139.124:8099/api/'
             // return 'http://www.api.jzbl.com/api/'
         }
@@ -17,27 +18,27 @@ exports.configUrl = {
     localUrl: (() => {
         // 测试环境
         if (process.env.NODE_ENV === 'demo') {
-            return 'http://www.demo.jzbl.com/api/'
+            return 'http://www.jzbl.com/'
         } else if (process.env.NODE_ENV === 'pro') {
         // 正式环境
             return 'http://www.jzbl.com/'
         } else {
         // 本地环境
-            // return 'http://127.0.0.1:8889'
+            // return 'http://192.168.10.14:8555'
             // return 'http://www.jzbl.com/'
-            return 'http://192.168.10.7:8889'
+            return 'http://192.168.10.7:8555'
         }
     })(),
     serverPort: (() => {
         // 测试端口
         if (process.env.NODE_ENV === 'demo') {
-            return '8889'
+            return '8555'
         } else if (process.env.NODE_ENV === 'pro') {
         // 正式端口
-            return '8889'
+            return '8555'
         } else {
         // 本地端口
-            return '8889'
+            return '8555'
         }
     })(),
     // 文件路径
