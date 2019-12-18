016ffc52e0054814b04cab4e97c03d3afbc89ee9
DESKTOP-OVJB0DA\sqliuen <sqliuen@163.com>
2019-12-18 18:58:41 +0800

    add

--- pages/User/myInvoice.vue
+++ pages/User/myInvoice.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-        <Title title="我的发票"/>
-        <div>
-        </div>
+        <Title :bottomLine="false"/>
+        <msgTab title="我的发票" :TabList="TabList"/>
+         <nuxt-child />
     </div>
 </template>
 <script>
 import Title from './components/title'
+import msgTab from './components/msgTab'
+import baseCard from '../../components/baseCard'
 export default {
     components: {
-        Title
+        Title,
+        msgTab,
+        baseCard
+    },
+    data () {
+        return {
+            TabList: [
+                {path: '/User/myInvoice', label: '我的发票'},
+                {path: '/User/myInvoice', label: '发票设置'},
+            ]
+        }
     }
 }
 </script>
+<style lang="less" scoped>
+  
+</style>
